e93a0c9444f439791b2e457be172d1d1459f73fb
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-10-28 11:22:47 -0500

    CSS for Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom"
 const Nav = () => {
     return (
         <div className="nav">
+            <img className="logo" src="https://i.etsystatic.com/22467704/r/il/7c21f6/3361056401/il_570xN.3361056401_lcbi.jpg"/>
+            <div className="navs">
             <Link to="/">Home</Link>
             <Link to="/signup">Sign In</Link>
+            </div>
         </div>
     )
 }
